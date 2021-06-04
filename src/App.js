2bbe7c9e1d5244be26a1f51e1a4fd79279427f90
Josue Arreola <47585614+josh231101@users.noreply.github.com>
2021-06-03 21:52:35 -0500

    Refactor to components CAP. 21 - Usando sistema de módulos

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React from "react";
 import "./styles.css";
+import TarjetaFruta from "./components/TarjetaFruta";
 
-class App extends React.Component {
-  // Una clase tiene métodos
-  // funciones
-  state = {
-    cantidad: 0
-  };
-
-  update = () => {
-    this.setState({ cantidad: this.state.cantidad + 1 });
-  };
-  substract = () => {
-    this.setState({ cantidad: this.state.cantidad - 1 });
-  };
-  refresh = () => {
-    this.setState({ cantidad: 0 });
-  };
-  render() {
-    const { edad, nombre } = this.props;
-    return (
-      <div>
-        <h3>Hola</h3>
-        <p>
-          Soy {nombre} y tengo {edad}
-        </p>
-        <h4>Cantidad de clicks: {this.state.cantidad}</h4>
-        <button onClick={this.update}>Add</button>
-        <button onClick={this.substract}> substract</button>
-        <button onClick={this.update.bind(this)}>
-          binding this in the function
-        </button>
-        <button onClick={this.refresh}>Reset</button>
-      </div>
-    );
-  }
-}
-
+const App = () => {
+  return (
+    <>
+      <TarjetaFruta name="Josue" edad={19} />
+      <TarjetaFruta name="DAvid" edad={20} />
+    </>
+  );
+};
 export default App;
---
+++ src/components/TarjetaFruta.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class App extends React.Component {
+  // Una clase tiene métodos
+  // funciones
+  state = {
+    cantidad: 0
+  };
+
+  update = () => {
+    this.setState({ cantidad: this.state.cantidad + 1 });
+  };
+  substract = () => {
+    this.setState({ cantidad: this.state.cantidad - 1 });
+  };
+  refresh = () => {
+    this.setState({ cantidad: 0 });
+  };
+  render() {
+    const { edad, nombre } = this.props;
+    return (
+      <div>
+        <h3>Hola</h3>
+        <p>
+          Soy {nombre} y tengo {edad}
+        </p>
+        <h4>Cantidad de clicks: {this.state.cantidad}</h4>
+        <button onClick={this.update}>Add</button>
+        <button onClick={this.substract}> substract</button>
+        <button onClick={this.update.bind(this)}>
+          binding this in the function
+        </button>
+        <button onClick={this.refresh}>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default App;
