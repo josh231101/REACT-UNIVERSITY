023a53981f050b769d6abdfd7921bf01941898fb
Josue Arreola <47585614+josh231101@users.noreply.github.com>
2021-06-03 21:48:33 -0500

    Using property initializers CAP - 20 : Usando property initializers para evitar bindear this en el constructor

--- src/App.js
+++ src/App.js
@@ -4,30 +4,19 @@ import "./styles.css";
 class App extends React.Component {
   // Una clase tiene métodos
   // funciones
-  constructor() {
-    // El constructor es el método que se ejecuta cuando se crea el componente
-    super();
-    // Binding this into our methods
-    // SMART WAY TO BIND THIS
-    const METHODS = ["update", "substract", "refresh"];
-    METHODS.forEach((method) => {
-      this[method] = this[method].bind(this);
-    });
+  state = {
+    cantidad: 0
+  };
 
-    this.state = {
-      cantidad: 0
-    };
-  }
-
-  update() {
+  update = () => {
     this.setState({ cantidad: this.state.cantidad + 1 });
-  }
-  substract() {
+  };
+  substract = () => {
     this.setState({ cantidad: this.state.cantidad - 1 });
-  }
-  refresh() {
+  };
+  refresh = () => {
     this.setState({ cantidad: 0 });
-  }
+  };
   render() {
     const { edad, nombre } = this.props;
     return (
